39894e0f254cd3aa50cdad33ad947df5b9ab3d00
dathwo@outlook.com <dathwo@outlook.com>
2020-05-05 16:28:33 -0700

    Updated MathDojo to include unit testing

--- Python/python/MathDojo.py
+++ Python/python/MathDojo.py
@@ -1,3 +1,5 @@
+import unittest
+
 class MathDojo:
     def __init__(self):
         self.result = 0
@@ -12,12 +14,29 @@ class MathDojo:
             self.result-=x
         return self
 
-md = MathDojo()
+class Tests(unittest.TestCase):    
+    def testAdd1(self):
+        self.assertEqual(self.md.add(2).add(2,5,1).subtract(3,2).result, 5)
 
-x = md.add(2).add(2,5,1).subtract(3,2).result
-print(x)
+    def testAdd2(self):
+        self.assertEqual(self.md.subtract(4, 1, 6).add(3).result, -8)
 
-md = MathDojo()
-sub = MathDojo()
-print(md.add(10).add(2).add(15).result)
-print(sub.subtract(14).subtract(12).subtract(1).result)
+    def testAdd3(self):
+        self.assertEqual(self.md.add(1,2,3,4,5,6,7).subtract(1,2,3,4,5,6,7).result, 0)
+
+    def setUp(self):
+        self.md = MathDojo()
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+# md = MathDojo()
+
+# x = md.add(2).add(2,5,1).subtract(3,2).result
+# print(x)
+
+# md = MathDojo()
+# sub = MathDojo()
+# print(md.add(10).add(2).add(15).result)
+# print(sub.subtract(14).subtract(12).subtract(1).result)
