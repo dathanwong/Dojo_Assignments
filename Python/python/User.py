c6ca7e76294edc9ba81f91849221a2db30a0254d
dathwo@outlook.com <dathwo@outlook.com>
2020-05-05 11:25:51 -0700

    Added chaining functionality to user

--- Python/python/User.py
+++ Python/python/User.py
@@ -5,6 +5,7 @@ class User:
     
     def makeDeposit(self,amount):
         self.balance += amount
+        return self
 
     def checkBalance(self, amount):
         if(self.balance < amount):
@@ -16,9 +17,10 @@ class User:
     def makeWithdrawal(self,amount):
         if(self.checkBalance(amount)):
             self.balance -= amount
+        return self
 
     def displayBalance(self):
-        print(f"Balance: {self.balance}")
+        print(f"{self.name} Balance: {self.balance}")
 
     def transferBalance(self, user, amount):
         if(self.checkBalance(amount)):
@@ -26,27 +28,18 @@ class User:
             user.balance += amount
             self.displayBalance()
             user.displayBalance()
+        return self
 
     def __repr__(self):
         return f"Name: {self.name} Balance: {self.balance}"
 
 jim = User("Jim")
-jim.makeDeposit(100)
-jim.makeDeposit(500)
-jim.makeDeposit(20)
-jim.displayBalance()
+jim.makeDeposit(100).makeDeposit(500).makeDeposit(20).displayBalance()
 
 dwight = User("Dwight")
-dwight.makeDeposit(10)
-dwight.makeDeposit(1000)
-dwight.makeWithdrawal(500)
-dwight.displayBalance()
+dwight.makeDeposit(10).makeDeposit(1000).makeWithdrawal(500).displayBalance()
 
 pam = User("Pam")
-pam.makeDeposit(400)
-pam.makeWithdrawal(10)
-pam.makeWithdrawal(50)
-pam.makeWithdrawal(30)
-pam.displayBalance()
+pam.makeDeposit(400).makeWithdrawal(10).makeWithdrawal(50).makeWithdrawal(30).displayBalance()
 
 jim.transferBalance(pam, 30)
