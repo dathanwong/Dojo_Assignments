38d815053bec7b55a090baab20f3815aa9b8a7d1
dathwo@outlook.com <dathwo@outlook.com>
2020-05-05 18:52:41 -0700

    Added first django project

---
+++ Python/my_environments/djangoPy3Env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dathanwong/OneDrive/Dojo_Assignments/Python/my_environments/djangoPy3Env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
