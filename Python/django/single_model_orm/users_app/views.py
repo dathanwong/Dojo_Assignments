205ba63ca7312a44244b60c24d24a424b05027d8
dathwo@outlook.com <dathwo@outlook.com>
2020-05-11 14:18:41 -0700

    Finished Users w/ Templates assignment

--- Python/django/single_model_orm/users_app/views.py
+++ Python/django/single_model_orm/users_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse, redirect
+from .models import User
 # Create your views here.
+def home(request):
+    context = {
+        "users": User.objects.all()
+    }
+    return render(request,"home.html", context)
+
+def addUser(request):
+    print(request.POST)
+    User.objects.create(first_name=request.POST["firstName"], last_name=request.POST["lastName"], email_address=request.POST["email"], age=request.POST["age"])
+    return redirect("/")
