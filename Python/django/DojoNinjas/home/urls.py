7aea9c49466af03762d785b5e5aca4c76f94ed3f
dathwo@outlook.com <dathwo@outlook.com>
2020-05-12 19:45:05 -0700

    Finished Dojos Ninjas assignment

--- Python/django/DojoNinjas/home/urls.py
+++ Python/django/DojoNinjas/home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('addDojo', views.addDojo),
+    path('addNinja', views.addNinja),
+    path('delete/<int:dojoId>', views.deleteDojo, name='deleteDojo')
 ]
--- Python/django/DojoNinjas/home/views.py
+++ Python/django/DojoNinjas/home/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home")
+    context = {
+        "dojos": Dojo.objects.all(),
+        "ninjas": Ninjas.objects.all()
+    }
+    return render(request, "home.html", context)
+
+def addDojo(request):
+    Dojo.objects.create(name=request.POST["name"], city = request.POST["city"], state = request.POST["state"])
+    return redirect('/')
+
+def addNinja(request):
+    Ninjas.objects.create(first_name=request.POST["first_name"], last_name=request.POST["last_name"], dojo_id = Dojo.objects.filter(name=request.POST["dojo"])[0])
+    return redirect('/')
+
+def deleteDojo(request, dojoId):
+    Dojo.objects.get(id=dojoId).delete()
+    print(f"Dojo {dojoId} deleted")
+    return redirect('/')
